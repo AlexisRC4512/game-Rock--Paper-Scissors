f3dec502a29f06e379fe30b0c87f2cd52120b758
AlexisRC <etherio4512@hotmail.com>
2021-11-26 17:47:06 -0500

    end

--- js/dom.js
+++ js/dom.js
@@ -2,7 +2,7 @@ import game from "./game.js";
 
 const d=document;
 
-d.addEventListener('click',(e)=>{
+d.addEventListener('DOMcontentLoaded',(e)=>{
     game("#paper","#rock","#scissors")
     e.preventDefault();
 })
